86895d59c77e0232e640ccedc7a8f4684f33e400
mgymrek <mgymrek@mit.edu>
2016-11-11 23:39:33 -0500

    updaing windows script

--- dna-replication-sandbox/readme.sh
+++ dna-replication-sandbox/readme.sh
@@ -6,4 +6,7 @@ PREFIX=wceGM12878
 GENOMEFA=/humgen/atgu1/fs03/wip/gymrek/genomes/hg19.fa
 GENOMEFILE=hg19.genome
 
+# Get windows
+bedtools makewindows -g ${GENOMEFILE} -w 10000 -s 5000 > hg19_windows_10_5.bed
+
 bsub -q week -eo gc.err -oo gc.out ./get_gc_corrected_readcounts.sh $BAMFILE $GCBINSIZE $PREFIX
