a03f8b2684b3211561c47009d28a13788ea18842
Yutong Qiu <yuq005@ucsd.edu>
2017-07-31 15:47:23 -0700

    modified script for WCE to be filtered

--- dna-replication-sandbox/get_binned_counts.sh
+++ dna-replication-sandbox/get_binned_counts.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
-BAMFILE=$1
-CHROM=$2
-WINDOWS=$3
-OUTDIR=$4
+BAMDIR=$1
+WINDOWS=$2
+OUTDIR=$3
 
-samtools view -b ${BAMFILE} chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 | \
-    samtools mpileup - | awk '{print $1 "\t" $2 "\t" $2+1 "\t" $4}' | \
-    intersectBed -a ${WINDOWS} -b stdin -wa -wb -sorted | cut -f 1,2,3,7 | \
-    bedtools groupby -g 1,2,3 -c 4 -o sum > ${OUTDIR}/$(basename ${BAMFILE} .bam).counts.bed
+###
+# BAM TO COUNTS.BED
+###
+for f in ${BAMDIR}/*
+do
+	samtools idxstats ${BAMFILE} | cut -f 1 | sed -n 2,20p | \
+		xargs samtools view -b ${BAMFILE} | \
+	    samtools mpileup - | awk '{print $1 "\t" $2 "\t" $2+1 "\t" $4}' | \
+	    intersectBed -a ${WINDOWS} -b stdin -wa -wb -sorted | cut -f 1,2,3,7 | \
+	    bedtools groupby -g 1,2,3 -c 4 -o sum > ${OUTDIR}/$(basename ${BAMFILE} .bam | cut -f 1 -d '.').counts.bed
+done
+
+###
+# FILTERING: get the peaks only
+###
+# for f in ${OUTDIR}/*
+# do
+# 	name=`basename $f | cut -f 1 -d '.' | cut -f 1,2,3 -d '_'`
+# 	intersectBed -wa -wb -sorted -a $f -b ./regions_sep/exp1_0hr_K4me3.regions.bed.sorted.rm | cut -f 1,2,3,4 | uniq > ./${OUTDIR}/peaksonly/$name.filtered.bed 
+# 	intersectBed -wa -wb -sorted -a ${OUTDIR}/peaksonly/$name.filtered.bed -b ./data/simon_cluster_mm9.bed | cut -f 5,6,7,8,4 | bedtools groupby -g 2,3,4,5 -c 1 -o sum > ./${OUTDIR}/peaksonly/$name.filtered.clustered.bed
+# done
+
+
+
+
 
